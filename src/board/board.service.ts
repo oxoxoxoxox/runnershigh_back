bfb46815d818e36efa10a195e440eb5a790d65b3
oxoxoxoxox <dudwlsaos132@naver.com>
2024-11-28 15:35:28 +0900

    [edit] team_Entity, User_Entity에 등록 되도록 로직 수정

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -4,30 +4,53 @@ import { Repository } from 'typeorm';
 import { BoardCreateDto } from './boarddto/req/boardCreateDto';
 import { BoardEntity } from '../Entitiy/board.entity';
 import { UserEntity } from '../Entitiy/user.entity';
+import { Team_entity } from '../Entitiy/team_entity';
 
 @Injectable()
 export class BoardService {
   constructor(
     @InjectRepository(BoardEntity)
     private readonly boardEntity: Repository<BoardEntity>,
+    @InjectRepository(Team_entity)
+    private readonly teamEntity: Repository<Team_entity>,
+    @InjectRepository(UserEntity)
+    private readonly userEntity: Repository<UserEntity>,
   ) {}
   async create(body: BoardCreateDto, req: Request) {
-    const userdata: UserEntity = new UserEntity();
-    userdata.id = req['user'].id;
-    const data: BoardEntity = new BoardEntity();
-    data.title = body.title;
-    data.contents = body.contents;
-    data.image_url = body.image_url;
-    data.gender = body.gender;
-    data.time = body.time;
-    data.people = 1;
-    data.status = true; // 신청가능
-    data.user = userdata;
-    await this.boardEntity.save(data);
-    return data;
+    const userID = req['user'].id;
+
+    const board: BoardEntity = new BoardEntity();
+    board.title = body.title;
+    board.contents = body.contents;
+    board.image_url = body.image_url;
+    board.gender = body.gender;
+    board.time = body.time;
+    board.people = 1;
+    board.status = true; // 신청가능
+
+    if (board.people == 4) {
+      //신청마감.
+      board.status = false;
+    }
+
+    board.user = userID;
+    await this.boardEntity.save(board);
+
+    const team = await this.teamEntity.findOne({
+      relations: {
+        users: true,
+      },
+      where: {
+        id: req['user'].id,
+      },
+    });
+    const user = await this.userEntity.findOneBy({ id: userID });
+    team.users = [user];
+    await this.teamEntity.save(team);
+    return board;
   }
   async search() {
-    const board = this.boardEntity.find({});
+    const board = this.boardEntity.find();
     return board;
   }
   async join() {}
