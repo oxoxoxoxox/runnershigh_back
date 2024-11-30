33bf1a9eb0f698e397f6e190d535c49c4b92082d
oxoxoxoxox <dudwlsaos132@naver.com>
2024-12-01 02:48:00 +0900

    [edit] env 경로 변경 및 configService 추가

--- mariadb-init/01-init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON mydatabase.* TO 'user'@'%';
-FLUSH PRIVILEGES;
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './Entitiy/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
-import { BoardService } from './board/board.service';
 import { BoardModule } from './board/board.module';
 import { BoardEntity } from './Entitiy/board.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { MissionList } from './Entitiy/mission_list';
 import { MissionEntity } from './Entitiy/mission.entity';
-import { MissionService } from './mission/mission.service';
 import { MissionModule } from './mission/mission.module';
 import { RangkingEntity } from './Entitiy/rangking.entity';
 import { RunningModule } from './running/running.module';
 import { RunningEntity } from './Entitiy/running.entity';
 import { RankingModule } from './ranking/ranking.module';
-import { RankingService } from './ranking/ranking.service';
 import { Team_entity } from './Entitiy/team_entity';
 
 @Module({
@@ -31,11 +28,11 @@ import { Team_entity } from './Entitiy/team_entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'mariadb',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
         entities: [
           UserEntity,
           BoardEntity,
