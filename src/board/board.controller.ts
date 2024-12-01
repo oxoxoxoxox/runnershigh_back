3671fda010b5239b162d72a0a1b32617b0f3a850
oxoxoxoxox <dudwlsaos132@naver.com>
2024-12-02 01:33:02 +0900

    [add] image 파일 업로드 로직 구현

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -5,23 +5,58 @@ import {
   UseGuards,
   Request,
   Get,
+  UseInterceptors,
+  UploadedFile,
+  Res,
 } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { BoardCreateDto } from './boarddto/req/boardCreateDto';
 import { AccessGuard } from '../jwtGuard/access.guard';
 import { BoardJoinDto } from './boarddto/req/boardJoin.Dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'node:path';
+import { Response } from 'express';
+
+interface FileParams {
+  fileName: string;
+}
 
 @UseGuards(AccessGuard)
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
+
   @Post('create')
-  async create(@Body() body: BoardCreateDto, @Request() req: Request) {
+  async create(
+    @Body() body: BoardCreateDto,
+    @UploadedFile() file: Express.Multer.File,
+    @Request() req: Request,
+  ) {
     return this.boardService.create(body, req);
   }
   @Get('search')
   async search() {
     return this.boardService.search();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          cb(null, `${file.originalname}`);
+        },
+      }),
+    }),
+  )
+  async uploadfile() {
+    return 'sss';
+  }
+  @Get('getFile')
+  getFile(@Res() res: Response, @Body() file: FileParams) {
+    res.sendFile(path.join(__dirname, '../uploads/' + file.fileName));
   }
   @Post('join')
   async join(@Body() body: BoardJoinDto, @Request() req: Request) {
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(path.join(__dirname, './uploads'));
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
